4d62f128c1cd9d4621ea7f07476d882ba71bb72f
Edward Newton <enewton@sentryds.com>
2021-06-08 11:31:02 -0700

    Added JsonResponse to return notes at /genus/{genusName}/notes

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -18,15 +20,48 @@ class GenusController extends Controller
      */
     public function showAction(string $genusName): Response
     {
+        return $this->render(
+            'genus/show.html.twig',
+            [
+                'name' => $genusName,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
         $notes = [
-            'note 1',
-            'note 2',
-            'note 3',
+            [
+                'id' => 1,
+                'username' => 'AquaPelham',
+                'avatarUri' => '/images/leanna.jpeg',
+                'note' => 'Octopus asked me a riddle, outsmarted me',
+                'date' => 'Dec. 10, 2015'
+            ],
+            [
+                'id' => 2,
+                'username' => 'AquaWeaver',
+                'avatarUri' => '/images/ryan.jpeg',
+                'note' => 'I counted 8 legs... as they wrapped around me',
+                'date' => 'Dec. 1, 2015'
+            ],
+            [
+                'id' => 3,
+                'username' => 'AquaPelham',
+                'avatarUri' => '/images/leanna.jpeg',
+                'note' => 'Inked!',
+                'date' => 'Aug. 20, 2015'
+            ],
         ];
 
-        return $this->render('genus/show.html.twig', [
-            'name' => $genusName,
+        $data = [
             'notes' => $notes,
-        ]);
+        ];
+
+        return new JsonResponse($data);
     }
 }
