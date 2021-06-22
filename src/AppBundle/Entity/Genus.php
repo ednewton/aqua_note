58323c6bdaefd08cf4d28ed00139ce37bb5f771b
Edward Newton <enewton@sentryds.com>
2021-06-22 10:57:29 -0700

    Chapter 4: Added other columns to Genus

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -25,10 +25,89 @@ class Genus
     private $name;
 
     /**
-     * @param mixed $name
+     * @ORM\Column(type="string")
      */
-    public function setName($name): void
+    private $subFamily;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $speciesCount;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $funFact;
+
+    /**
+     * @return string
+     */
+    public function getSubFamily(): string
+    {
+        return $this->subFamily;
+    }
+
+    /**
+     * @param string $subFamily
+     */
+    public function setSubFamily(string $subFamily): void
+    {
+        $this->subFamily = $subFamily;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpeciesCount(): int
+    {
+        return $this->speciesCount;
+    }
+
+    /**
+     * @param int $speciesCount
+     */
+    public function setSpeciesCount(int $speciesCount): void
+    {
+        $this->speciesCount = $speciesCount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFunFact(): string
+    {
+        return $this->funFact;
+    }
+
+    /**
+     * @param string $funFact
+     */
+    public function setFunFact(string $funFact): void
+    {
+        $this->funFact = $funFact;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
     {
         $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
     }
 }
