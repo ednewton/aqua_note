6778e21ec9b49dc3c87465dba198e61ff2796b15
Edward Newton <enewton@sentryds.com>
2021-06-22 09:43:17 -0700

    Chapter 11: final notes on routing

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function homepageAction()
+    {
+        return $this->render('main/homepage.html.twig');
+    }
+}
